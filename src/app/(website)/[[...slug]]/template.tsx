1dbbac6a22cb5dee83f5fe87daeaed6bbc8b613e
Judah Sullivan <judahsullivan.dev@gmail.com>
2025-11-04 15:25:15 -0600

    🚀deployement testing

--- src/app/(website)/[[...slug]]/template.tsx
+++ src/app/(website)/[[...slug]]/template.tsx
@@ -10,6 +10,5 @@ export default function Template({
 }: {
   children: ReactNode;
 }) {
-  console.log("Template Layout Is Active")
   return <>{children}</>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import { ReactNode } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
+
+
+
+
+
 export default async function RootLayout({
   children,
   lang,
